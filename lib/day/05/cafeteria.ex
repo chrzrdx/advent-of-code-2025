25082ad594aaa446dbcb4a07bf3010748cf08dc4
Abhinandan Panigrahi <chrzrdx@gmail.com>
2025-12-04 23:18:22 -0700

    Day 5 Part 2

--- lib/day/05/cafeteria.ex
+++ lib/day/05/cafeteria.ex
@@ -15,16 +15,37 @@ defmodule AdventOfCode2025.Day05.Cafeteria do
       ranges_str
       |> String.split("\n", trim: true)
       |> Enum.map(fn line ->
-        [a, b] = String.split(line, "-", trim: true) |> Enum.map(&String.to_integer/1)
-        a..b
+        line
+        |> String.split("-")
+        |> Enum.map(&String.to_integer/1)
+        |> List.to_tuple()
       end)
+      |> merge_ranges()
 
     ingredients =
       ingredients_str
       |> String.split("\n", trim: true)
       |> Enum.map(&String.to_integer/1)
+      |> Enum.sort()
 
     %__MODULE__{ranges: ranges, ingredients: ingredients}
+  end
+
+  defp merge_ranges(unmerged) do
+    unmerged
+    |> Enum.sort()
+    |> then(fn [first | unmerged_rest] -> merge_ranges([first], unmerged_rest) end)
+    |> Enum.map(fn {a, b} -> a..b end)
+  end
+
+  defp merge_ranges(merged, []), do: Enum.reverse(merged)
+
+  defp merge_ranges([{a, b} | merged_rest], [{c, d} | unmerged_rest]) do
+    cond do
+      c > b -> merge_ranges([{c, d}, {a, b} | merged_rest], unmerged_rest)
+      d <= b -> merge_ranges([{a, b} | merged_rest], unmerged_rest)
+      true -> merge_ranges([{a, d} | merged_rest], unmerged_rest)
+    end
   end
 
   def solve_p1(%__MODULE__{ranges: ranges, ingredients: ingredients}) do
@@ -33,7 +54,7 @@ defmodule AdventOfCode2025.Day05.Cafeteria do
     end)
   end
 
-  def solve_p2(%__MODULE__{}) do
-    0
+  def solve_p2(%__MODULE__{ranges: ranges}) do
+    Enum.sum_by(ranges, &Range.size/1)
   end
 end
--- test/day/05_test.exs
+++ test/day/05_test.exs
@@ -1,12 +1,19 @@
 defmodule AdventOfCode2025.Day05Test do
   use ExUnit.Case
   import AdventOfCode2025.Day05, only: [solve_p1: 1, solve_p2: 1]
+  import AdventOfCode2025.Day05.Cafeteria, only: [from_file: 1]
 
   @p1 "test/day/05/p1.txt"
   @p2 "test/day/05/p2.txt"
   @puzzle "test/day/05/puzzle.txt"
 
   describe "part 1" do
+    test "from_file" do
+      data = from_file(@p1)
+      assert data.ingredients == [1, 5, 8, 11, 17, 32]
+      assert data.ranges == [3..5, 10..20]
+    end
+
     test "example" do
       assert solve_p1(@p1) == 3
     end
@@ -17,14 +24,12 @@ defmodule AdventOfCode2025.Day05Test do
   end
 
   describe "part 2" do
-    @tag :skip
     test "example" do
       assert solve_p2(@p2) == 14
     end
 
-    @tag :skip
     test "puzzle" do
-      assert solve_p2(@puzzle) == 0
+      assert solve_p2(@puzzle) == 358_155_203_664_116
     end
   end
 end
