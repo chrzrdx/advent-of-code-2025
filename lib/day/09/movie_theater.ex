ea37169498bc793fe64cb0bbe97b5fbd9dfd9cb8
Abhinandan Panigrahi <chrzrdx@gmail.com>
2025-12-30 13:14:53 -0700

    minor refactor Day 9 Part 1

--- lib/day/09/movie_theater.ex
+++ lib/day/09/movie_theater.ex
@@ -3,10 +3,10 @@ defmodule AdventOfCode2025.Day09.MovieTheater do
   Utility functions for Day 09.
   """
 
-  defstruct [:data]
+  defstruct [:points]
 
   def from_file(filename) do
-    data =
+    points =
       filename
       |> File.read!()
       |> String.split("\n", trim: true)
@@ -16,22 +16,22 @@ defmodule AdventOfCode2025.Day09.MovieTheater do
         |> Enum.map(&String.to_integer/1)
       end)
       |> Enum.map(fn [x, y] -> {x, y} end)
-      |> Enum.sort()
 
-    %__MODULE__{data: data}
+    %__MODULE__{points: points}
   end
 
-  def solve_p1(%__MODULE__{data: data}) do
-    for {x1, y1} <- data,
-        {x2, y2} <- data,
+  def solve_p1(%__MODULE__{points: points}) do
+    sorted_points = Enum.sort(points)
+
+    for {x1, y1} <- sorted_points,
+        {x2, y2} <- sorted_points,
         x1 < x2 do
       abs((x2 - x1 + 1) * (y2 - y1 + 1))
     end
     |> Enum.max()
   end
 
-  def solve_p2(%__MODULE__{data: data}) do
-    # TODO: Implement part 2
-    data
+  def solve_p2(%__MODULE__{points: points}) do
+    points
   end
 end
